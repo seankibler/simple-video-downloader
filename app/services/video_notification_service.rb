fbaae35ad740f66cf53bbc0f5ccaf53fe6e9099e
kibbyson <63254+seankibler@users.noreply.github.com>
2026-05-01 07:02:04 -0400

    Generate notifications when users download videos.

--- app/services/video_notification_service.rb
+++ app/services/video_notification_service.rb
@@ -6,6 +6,21 @@ class VideoNotificationService
     end
 
     def notify
-        Ntfy.new(NTFY_TOPIC).send_notification("Video Downloaded", "#{@video.user.username} downloaded #{@video.title} from #{@video.link} at #{@video.updated_at.strftime("%Y-%m-%d %H:%M:%S")}")
+        message = <<~MESSAGE
+            #{@video.user.username} downloaded #{@video.title} from #{@video.link} at #{@video.updated_at.strftime("%Y-%m-%d %H:%M:%S")}
+            --------------------------------
+            Video: #{@video.title}
+            Link: #{@video.link}
+            Status: #{@video.status}
+            --------------------------------
+            Video Utilization: #{@video.user.videos.count} / #{User::MAX_VIDEOS}
+            Storage Utilization: #{@video.user.total_storage_used} / #{User::MAX_STORAGE}
+            --------------------------------
+            Total Storage Used: #{User.all.sum { |user| user.total_storage_used }}
+            Total Videos: #{User.all.sum { |user| user.videos.count }}
+            --------------------------------
+        MESSAGE
+
+        Ntfy.new(NTFY_TOPIC).send_notification("Video Downloaded", message)
     end
 end
